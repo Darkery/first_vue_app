2b0052aed2cc0e71c091e8aab63aa427610bf2c1
Jing Li <jing.li08@sap.com>
2020-08-12 17:49:15 +0800

    Basic UI

---
+++ src/plugins/element.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import {
+  Button,
+  Upload,
+  Input,
+  Dialog,
+  Form,
+  FormItem,
+  Message,
+  MessageBox,
+  Row,
+  Col
+  // Icon,
+  // Card,
+  // Table,
+  // TableColumn,
+  // TabPane,
+  // Tabs,
+  // Tag,
+} from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
+Vue.use(Button)
+Vue.use(Upload)
+Vue.use(Input)
+Vue.use(Dialog)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Row)
+Vue.use(Col)
+// Vue.use(Icon)
+// Vue.use(Card)
+// Vue.use(Table)
+// Vue.use(TableColumn)
+// Vue.use(TabPane)
+// Vue.use(Tabs)
+// Vue.use(Tag)
+
+Vue.prototype.$message = Message
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
